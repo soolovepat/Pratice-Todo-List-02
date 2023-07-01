39716a092a61d81366cf8af2789404579e5774c4
Sujin Lee <soolovepat@naver.com>
2023-07-01 19:24:54 +0900

    Initial commit

---
+++ src/pages/components/TodoCreate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoCreate() {
+  return <div>TodoCreate</div>;
+}
+
+export default TodoCreate;
---
+++ src/pages/components/Todohead.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Todohead() {
+  return <div>Todohead</div>;
+}
+
+export default Todohead;
