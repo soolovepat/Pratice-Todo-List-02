6a0514ccb361791655932ad53fcc15ec1100a09d
Sujin Lee <soolovepat@naver.com>
2023-07-01 21:27:24 +0900

    Add redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Todohead from "./components/Todohead";
+import TodoCreate from "./components/TodoCreate";
+import TodoList from "./components/TodoList";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Todohead />
+      <TodoCreate />
+      <TodoList />
+    </>
+  );
 }
 
 export default Home;
