1fd8c45172e7640d525aedb2f3ceb0078c915e13
Sujin Lee <soolovepat@naver.com>
2023-07-01 19:47:22 +0900

    Add GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <GlobalStyle />
+    </main>
+  );
 }
 
 export default App;
